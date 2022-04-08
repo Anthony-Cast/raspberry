3e4fa12a2ea8f7881aad64bfae00c8deeda0b9c7
Anthony-Cast <a20155533@pucp.edu.pe>
2022-04-08 11:41:54 -0500

    cambios en el bash

--- html/bash.sh
+++ html/bash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 cd html
 sqlite3 raspberry.db "create table if not exists registros(id integer primary key autoincrement,anio text not null,mes text not null,dia text not null,hora text not null,archivo text not null,formato not null);" ".quit"
 
@@ -16,19 +17,29 @@ grep -E '.pdf HTTP|.mp4 HTTP' /var/log/nginx/kolibri_uwsgi.log | while read -r l
 	anio=${line[@]:26:4}
 	hora=${line[@]:31:8}
 	archivo=${line[@]:73:36}
-	formato=${line[@]:106:3}	
+	formato=${line[@]:106:3}
+
+	cd /~/.kolibri/content/databases
+	ls | grep -E '.sqlite3' | while read -r line ; do
+
+		sqlite3 $line 
+		titulo="select title from content_contentnode where content_id=$archivo"
+
+		if [ $titulo ]
+		then
+		".quit"
+		fi
+	done
+
 	if [ $diaActual == $dia ] && [ $mesActual == $mes ]
 	then
-		echo $anio,$mes,$dia,$hora,$archivo,$formato >> archivo.txt
-		sqlite3 raspberry.db "insert into registros(anio,mes,dia,hora,archivo,formato) values('$anio','$mes','$dia','$hora','$archivo','$formato');" ".quit"
+		echo $anio,$mes,$dia,$hora,$titulo,$formato >> archivo.txt
+		sqlite3 raspberry.db "insert into registros(anio,mes,dia,hora,archivo,formato) values('$anio','$mes','$dia','$hora','$titulo','$formato');" ".quit"
 	fi
 done
 
 sqlite3 raspberry.db "select * from registros;"
 
-<<'COMMENT'
-cd /~/.kolibri/content/databases
-ls | grep -E '.sqlite3' | while read -r line ; do
-	echo 'linea : '$line
-done
-COMMENT
+
+
+
